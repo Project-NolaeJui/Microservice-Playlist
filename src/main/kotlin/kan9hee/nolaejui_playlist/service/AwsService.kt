97f7939ad28a387c4546de6d178d0c2915bde343
han <hcsy1219@gmail.com>
2025-05-05 19:14:10 +0900

    refactor: Simplify aws s3 data transfer logic

--- src/main/kotlin/kan9hee/nolaejui_playlist/service/AwsService.kt
+++ src/main/kotlin/kan9hee/nolaejui_playlist/service/AwsService.kt
@@ -8,46 +8,32 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
 import java.time.Duration
-import java.util.*
 
 @Service
 class AwsService(private val s3ValueConfig: S3ValueConfig,
                  private val s3Presigner: S3Presigner,
                  private val s3Client: S3Client) {
 
-    fun deleteMusicS3(musicName:String){
-        val deleteRequest = DeleteObjectRequest.builder()
-            .bucket(s3ValueConfig.bucket)
-            .key(s3ValueConfig.folder + musicName)
-            .build()
-
-        s3Client.deleteObject(deleteRequest)
+    fun deleteMusicS3(musicName: String) {
+        s3Client.deleteObject(
+            DeleteObjectRequest.builder()
+                .bucket(s3ValueConfig.bucket)
+                .key(s3ValueConfig.folder + musicName)
+                .build()
+        )
     }
 
-    fun getPreSignedUrl(fileName:String): String {
-        val fullPath = createPath(fileName)
-
+    fun getPreSignedUrl(fileName: String): String {
         val putObjectRequest = PutObjectRequest.builder()
             .bucket(s3ValueConfig.bucket)
-            .key(fullPath)
+            .key(s3ValueConfig.folder + fileName)
             .build()
 
         val presignRequest = PutObjectPresignRequest.builder()
-            .signatureDuration(Duration.ofMinutes(getPreSignedUrlExpiration()))
+            .signatureDuration(Duration.ofMinutes(1))
             .putObjectRequest(putObjectRequest)
             .build()
 
-        val generatedPreSignedUrlRequest = s3Presigner.presignPutObject(presignRequest)
-        return generatedPreSignedUrlRequest.url().toString()
-    }
-
-    private fun getPreSignedUrlExpiration(): Long {
-        val expiration = Date()
-        val expTimeMillis = expiration.time + 1000 * 60
-        return expTimeMillis
-    }
-
-    private fun createPath(fileName: String): String {
-        return java.lang.String.format("%s%s", s3ValueConfig.folder, fileName)
+        return s3Presigner.presignPutObject(presignRequest).url().toString()
     }
 }
