464d77933001d6c657b79f14849264f5320ca879
han <hcsy1219@gmail.com>
2025-05-05 19:16:19 +0900

    refactor: Simplify aws s3 config

--- src/main/kotlin/kan9hee/nolaejui_playlist/config/S3Config.kt
+++ src/main/kotlin/kan9hee/nolaejui_playlist/config/S3Config.kt
@@ -12,30 +12,18 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner
 class S3Config(private val s3ValueConfig: S3ValueConfig) {
 
     @Bean
-    fun s3Client(): S3Client {
-        return S3Client.builder()
-            .region(
-                Region.of(s3ValueConfig.region)
-            )
-            .credentialsProvider(
-                StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(s3ValueConfig.accessKey, s3ValueConfig.secretKey)
-                )
-            )
-            .build()
-    }
+    fun s3Client(): S3Client = S3Client.builder()
+        .region(Region.of(s3ValueConfig.region))
+        .credentialsProvider(commonAwsCredentialsProvider())
+        .build()
 
     @Bean
-    fun s3Presigner(): S3Presigner {
-        return S3Presigner.builder()
-            .region(
-                Region.of(s3ValueConfig.region)
-            )
-            .credentialsProvider(
-                StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(s3ValueConfig.accessKey, s3ValueConfig.secretKey)
-                )
-            )
-            .build()
-    }
+    fun s3Presigner(): S3Presigner = S3Presigner.builder()
+        .region(Region.of(s3ValueConfig.region))
+        .credentialsProvider(commonAwsCredentialsProvider())
+        .build()
+
+    private fun commonAwsCredentialsProvider() = StaticCredentialsProvider.create(
+        AwsBasicCredentials.create(s3ValueConfig.accessKey, s3ValueConfig.secretKey)
+    )
 }
